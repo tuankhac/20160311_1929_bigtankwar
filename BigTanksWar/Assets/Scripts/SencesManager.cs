c89a2c7d05b21e2e1b66687780ed56878f362b50
LoserJAV <baodaihd89@gmail.com>
2016-03-13 16:37:46 +0700

    update script and add new gamestart sence

---
+++ BigTanksWar/Assets/Scripts/SencesManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SencesManager : MonoBehaviour {
+
+	public void LoadGamePlaySence () {
+		Application.LoadLevel ("Sences/GamePlayer");
+	}
+}
