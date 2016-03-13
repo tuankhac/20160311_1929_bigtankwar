b524e0fef6130186b9ff15c0a0fcf535434319b3
Tuan Khac <Tuan Khac>
2016-03-13 08:01:47 +0700

    add

--- BigTanksWar/Assets/Scripts/Tank/TankShooting.cs
+++ BigTanksWar/Assets/Scripts/Tank/TankShooting.cs
@@ -8,18 +8,18 @@ namespace Complete
         public int m_PlayerNumber = 1;              // Used to identify the different players.
         public Rigidbody m_Shell;                   // Prefab of the shell.
         public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
-       // public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
+        public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
        // public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
       //  public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
       //  public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
-        public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
-        public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
+        public float m_MinLaunchForce = 30f;        // The force given to the shell if the fire button is not held.
+        public float m_MaxLaunchForce = 60f;        // The force given to the shell if the fire button is held for the max charge time.
         public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
 
 
         private string m_FireButton;                // The input axis that is used for launching shells.
         private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
-        private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
+        private float m_ChargeSpeed = 0;                // How fast the launch force increases, based on the max charge time.
         private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
 		private bool isDown = false;
 		private bool isUp = false;
@@ -37,7 +37,7 @@ namespace Complete
             m_FireButton = "Fire" + m_PlayerNumber;
 
             // The rate that the launch force charges up is the range of possible forces by the max charge time.
-           // m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+            m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
         }
 		void Update(){
 			//keyboardFire ();
