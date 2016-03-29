5fd579f40a9346f31eb1447798a7eec6faef9542
tuankhac <vutuankhac@gmail.com>
2016-03-29 13:47:26 +0700

    update cho ko loi

--- BigTanksWar/Assets/Scripts/Tank/TankShooting.cs
+++ BigTanksWar/Assets/Scripts/Tank/TankShooting.cs
@@ -11,11 +11,11 @@ namespace Complete
 		// Prefab of the shell.
 		public Transform m_FireTransform;
 		// A child of the tank where the shells are spawned.
-		public Slider m_AimSlider;
+		//public Slider m_AimSlider;
 		// A child of the tank that displays the current launch force.
-		// public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
-		//  public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
-		//  public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
+		public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
+		public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
+		public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
 		public float m_MinLaunchForce = 30f;
 		// The force given to the shell if the fire button is not held.
 		public float m_MaxLaunchForce = 60f;
@@ -26,20 +26,19 @@ namespace Complete
 
 		private string m_FireButton;
 		// The input axis that is used for launching shells.
-		private float m_CurrentLaunchForce;
+		protected float m_CurrentLaunchForce;
 		// The force that will be given to the shell when the fire button is released.
-		private float m_ChargeSpeed = 0;
+		protected float m_ChargeSpeed = 0;
 		// How fast the launch force increases, based on the max charge time.
-		private bool m_Fired;
+		protected bool m_Fired;
 		// Whether or not the shell has been launched with this button press.
-		private bool isDown = false;
-		private bool isUp = false;
+
 
 		private void OnEnable ()
 		{
 			// When the tank is turned on, reset the launch force and the UI
 			m_CurrentLaunchForce = m_MinLaunchForce;
-			// m_AimSlider.value = m_MinLaunchForce;
+			//m_AimSlider.value = m_MinLaunchForce;
 		}
 
 
@@ -52,109 +51,7 @@ namespace Complete
 			m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
 		}
 
-		void Update ()
-		{
-			if (isDown) {
-				touchFire ();
-			}
-			if (isUp) {
-				Fire ();
-				isUp = false;
-			}
-
-
-		}
-
-		public void touchDown ()
-		{
-			isDown = true;
-			isUp = false;
-		}
-
-		public void touchUp ()
-		{
-			isDown = false;
-			isUp = true;
-		}
-		private void touchFire ()
-		{
-			// The slider should have a default value of the minimum launch force.
-			//m_AimSlider.value = m_MinLaunchForce;
-			// If the max force has been exceeded and the shell hasn't yet been launched...
-			if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
-				// ... use the max force and launch the shell.
-				m_CurrentLaunchForce = m_MaxLaunchForce;
-				Fire ();
-			}
-
-			// Otherwise, if the fire button has just started being pressed...
-			else if (Input.GetMouseButtonDown (0)) {
-				// ... reset the fired flag and reset the launch force.
-				m_Fired = false;
-				m_CurrentLaunchForce = m_MinLaunchForce;
-
-				// Change the clip to the charging clip and start it playing.
-				//m_ShootingAudio.clip = m_ChargingClip;
-				//m_ShootingAudio.Play ();
-
-			}
-			// Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-			else if (Input.GetMouseButton (0) && !m_Fired) {
-				// Increment the launch force and update the slider.
-				m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-
-				//m_AimSlider.value = m_CurrentLaunchForce;
-
-			}
-			// Otherwise, if the fire button is released and the shell hasn't been launched yet...
-			else if (Input.GetMouseButtonUp (0) && !m_Fired) {
-				// ... launch the shell.
-				Fire ();
-
-			}
-		}
-
-		private void keyboardFire ()
-		{
-			// The slider should have a default value of the minimum launch force.
-			//m_AimSlider.value = m_MinLaunchForce;
-
-			// If the max force has been exceeded and the shell hasn't yet been launched...
-			if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
-				// ... use the max force and launch the shell.
-				m_CurrentLaunchForce = m_MaxLaunchForce;
-				Fire ();
-				Debug.Log ("runs on");
-			}
-			// Otherwise, if the fire button has just started being pressed...
-			else if (Input.GetButtonDown (m_FireButton)) {
-				// ... reset the fired flag and reset the launch force.
-				m_Fired = false;
-				m_CurrentLaunchForce = m_MinLaunchForce;
-
-				// Change the clip to the charging clip and start it playing.
-				//m_ShootingAudio.clip = m_ChargingClip;
-				//m_ShootingAudio.Play ();
-				Debug.Log ("run ons");
-			}
-			// Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-			else if (Input.GetButton (m_FireButton) && !m_Fired) {
-				// Increment the launch force and update the slider.
-				m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-
-				//m_AimSlider.value = m_CurrentLaunchForce;
-				Debug.Log ("run ");
-			}
-			// Otherwise, if the fire button is released and the shell hasn't been launched yet...
-			else if (Input.GetButtonUp (m_FireButton) && !m_Fired) {
-				// ... launch the shell.
-				Debug.Log ("run on");
-				Fire ();
-			}
-
-		}
-
-		private void Fire ()
+		protected void Fire ()
 		{
 			// Set the fired flag so only Fire is only called once.
 			m_Fired = true;
@@ -167,8 +64,8 @@ namespace Complete
 			shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
 
 			// Change the clip to the firing clip and play it.
-			// m_ShootingAudio.clip = m_FireClip;
-			//m_ShootingAudio.Play ();
+			 m_ShootingAudio.clip = m_FireClip;
+			m_ShootingAudio.Play ();
 
 			// Reset the launch force.  This is a precaution in case of missing button events.
 			m_CurrentLaunchForce = m_MinLaunchForce;
