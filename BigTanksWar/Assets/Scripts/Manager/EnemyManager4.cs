4310efabe8773a919fea0e0d2d6793998a7f4fec
LoserJAV <baodaihd89@gmail.com>
2016-05-08 00:04:24 +0700

    create new script

---
+++ BigTanksWar/Assets/Scripts/Manager/EnemyManager4.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager4 : MonoBehaviour {
+	private bool isMove = false;
+
+	void Update () {
+		if (Vector3.Distance (transform.position, GameObject.FindGameObjectWithTag ("Player").transform.position) < 44) {
+			if (!isMove) {
+				GameObject[] _enemy = GameObject.FindGameObjectsWithTag ("Enemy");
+				GameObject[] _point = GameObject.FindGameObjectsWithTag ("EnemyZone4");
+				for (int i = 0; i < _point.Length; i++) {
+					Vector3 _temp = _point [i].transform.position;
+					_enemy [i].transform.position = _temp;
+				}
+				isMove = true;
+			}
+		} else {
+			if (isMove) {
+				GameObject[] _enemy = GameObject.FindGameObjectsWithTag ("Enemy");
+				GameObject[] _point = GameObject.FindGameObjectsWithTag ("EnemyGlobal");
+				for (int i = 0; i < _point.Length; i++) {
+					Vector3 _temp = _point [i].transform.position;
+					_enemy [i].transform.position = _temp;
+				}
+				isMove = false;
+			}
+		}
+	}
+}
