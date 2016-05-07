8235df59300b3f6524564ae0177306c13d0b97ca
LoserJAV <baodaihd89@gmail.com>
2016-05-07 22:07:31 +0700

    create zone script

---
+++ BigTanksWar/Assets/Scripts/Zone/GetZoneByPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetZoneByPlayer : MonoBehaviour {
+	public GameObject player;
+	public ParticleSystem particle;
+
+	void Start () {
+		particle.Play ();
+	}
+
+	void Update () {
+		if (Vector3.Distance (transform.position, player.transform.position) < 5) {
+			particle.startColor = Color.blue;
+		}
+	}
+}
